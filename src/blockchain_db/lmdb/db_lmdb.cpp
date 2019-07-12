9b305bbba0ec5232edc8c00f3ece6102c05893ec
Max Base <MaxBaseCode@Gmail.com>
2019-07-12 10:46:22 -0700

    Check the src/blockchain_db directory...

--- src/blockchain_db/lmdb/db_lmdb.cpp
+++ src/blockchain_db/lmdb/db_lmdb.cpp
@@ -1,9 +1,10 @@
+// Copyright (c) 2018, Ombre Cryptocurrency Project
 // Copyright (c) 2018, Ryo Currency Project
 // Portions copyright (c) 2014-2018, The Monero Project
 // Portions of this file are available under BSD-3 license. Please see ORIGINAL-LICENSE for details
 // All rights reserved.
 //
-// Ryo changes to this code are in public domain. Please note, other licences may apply to the file.
+// Ombre changes to this code are in public domain. Please note, other licences may apply to the file.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
@@ -31,8 +32,8 @@
 #include "ringct/rctOps.h"
 #include "string_tools.h"
 
-//#undef RYO_DEFAULT_LOG_CATEGORY
-//#define RYO_DEFAULT_LOG_CATEGORY "blockchain.db.lmdb"
+//#undef OMBRE_DEFAULT_LOG_CATEGORY
+//#define OMBRE_DEFAULT_LOG_CATEGORY "blockchain.db.lmdb"
 
 #if defined(__i386) || defined(__x86_64)
 #define MISALIGNED_OK 1
@@ -3342,7 +3343,7 @@ void BlockchainLMDB::fixup()
 	ptr = (char *)k.mv_data;                                                                    \
 	ptr[sizeof(name) - 2] = 's'
 
-#define LOGIF(y) if(ELPP->vRegistry()->allowed(y, RYO_DEFAULT_LOG_CATEGORY))
+#define LOGIF(y) if(ELPP->vRegistry()->allowed(y, OMBRE_DEFAULT_LOG_CATEGORY))
 
 void BlockchainLMDB::migrate_0_1()
 {
