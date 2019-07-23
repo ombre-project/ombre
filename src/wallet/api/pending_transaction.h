88d642f2eaaf4bd17eec6d8a738e6b5341dcc991
alirezahassanbeigi <32670224+alirezahassanbeigi@users.noreply.github.com>
2019-07-23 13:36:09 +0430

    Update pending_transaction.h

--- src/wallet/api/pending_transaction.h
+++ src/wallet/api/pending_transaction.h
@@ -19,7 +19,7 @@
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
-
+//
 #include "wallet/api/wallet2_api.h"
 #include "wallet/wallet2.h"
 
